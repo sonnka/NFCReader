58b6ae175c1a17b6491627844528b1212dc88d56
sonnka <sonyakazanceva1331@gmail.com>
2024-03-20 11:15:14 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,28 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'NFCReader';
+
+  async read(){
+    alert("User clicked scan button");
+
+    const ndef = new NDEFReader();
+    await ndef
+      .scan()
+      .then(() => {
+        alert("Scan started");
+
+        ndef.onreadingerror = () => {
+          alert("Cannot read data from the NFC tag. Try another one?");
+        };
+
+        ndef.onreading = (event) => {
+          alert('Serial Number:' + event.serialNumber);
+          alert('Records: ' + event.message.records.toString());
+        };
+
+      }).catch((error) => {
+          alert(error);
+      });
+  }
+
 }
